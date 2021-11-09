a36272d85934ee6c48c066812e61a5db377d1dc7
liujiaxing7 <1445101363@qq.com>
2021-11-09 13:43:27 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,28 +1,14 @@
-#include <iostream>
 #include "utils/yolov3.h"
-#include "utils/net.h"
 #if defined(USE_NCNN_SIMPLEOCV)
 #include "simpleocv.h"
 #else
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include "utils/yolov3.h"
-#include <opencv2/imgproc/imgproc.hpp>
 #endif
 #include <stdio.h>
 #include <vector>
-#include "time.h"
-#if defined(USE_NCNN_SIMPLEOCV)
-#include "simpleocv.h"
-#else
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#endif
-#include <stdio.h>
-#include <vector>
-#include "time.h"
-#include "utils/yolov3.h"
+
+
 int main(int argc, char** argv) {
     if (argc != 2) {
         fprintf(stderr, "Usage: %s [imagepath]\n", argv[0]);
