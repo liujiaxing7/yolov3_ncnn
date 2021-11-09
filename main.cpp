9e4372f13b207816e2f1b80189bcf3365e7bfeae
liujiaxing7 <1445101363@qq.com>
2021-11-09 12:24:58 +0800

    update

--- main.cpp
+++ main.cpp
@@ -23,19 +23,16 @@
 #include <vector>
 #include "time.h"
 #include "utils/yolov3.h"
-int main(int argc, char** argv)
-{
-    if (argc != 2)
-    {
+int main(int argc, char** argv) {
+    if (argc != 2) {
         fprintf(stderr, "Usage: %s [imagepath]\n", argv[0]);
         return -1;
     }
 
-    const char* imagepath = argv[1];
+    const char *imagepath = argv[1];
 
     cv::Mat m = cv::imread(imagepath, 1);
-    if (m.empty())
-    {
+    if (m.empty()) {
         fprintf(stderr, "cv::imread %s failed\n", imagepath);
         return -1;
     }
